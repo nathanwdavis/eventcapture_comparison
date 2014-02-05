892846ac9eead4369c5bd29c4a1dbb8ec647dc6f
Nathan D <nathan@courseload.com>
2014-02-05 13:06:41 -0500

    Validation of POST data with a precondition

--- clj/eventcapture/project.clj
+++ clj/eventcapture/project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.6"]
+                 [org.clojure/data.json "0.2.4"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler eventcapture.handler/app}
   :profiles
--- clj/eventcapture/src/eventcapture/handler.clj
+++ clj/eventcapture/src/eventcapture/handler.clj
@@ -1,14 +1,21 @@
 (ns eventcapture.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route])
+  (:require [clojure.data.json :as json])
+  (:require [clojure.string :as strings]))
+
+(defn now [] (new java.util.Date))
 
 (defn handle-event-post [event-type ext-ref user-ref data]
-  (println event-type)
-  (str event-type))
+  {:pre [(string? event-type) 
+         (not (strings/blank? event-type))
+         (string? data)
+         (json/read-str data)]}
+  (str event-type "|" ext-ref "|" user-ref "|" (json/read-str data)))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "It works!")
   (POST "/capture" [event-type ext-ref user-ref data]
         (handle-event-post event-type ext-ref user-ref data))
   (route/resources "/")
