a3e1d37e92ad622d232a023224326ac83430d950
Nathan D <nathan@courseload.com>
2014-02-06 18:04:32 -0500

    Handler for POST to /capture, but doesn't do anything yet.

--- golang/eventcaptureserver/eventcaptureserver.go
+++ golang/eventcaptureserver/eventcaptureserver.go
@@ -4,10 +4,17 @@ import (
 	"github.com/codegangsta/martini"
 )
 
+func routes(app *martini.Martini) {
+
+}
+
 func main() {
 	m := martini.Classic()
 	m.Get("/", func() string {
 		return "It works!"
+	})
+	m.Post("/capture", func(params martini.Params) string {
+		return ""
 	})
 	m.Run()
 }
